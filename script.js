19ce2a52cd3f46433de82c523437a16a54f9f405
Matt <m.delcampo@share.epsb.ca>
2024-10-18 15:09:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function getStatement1() {
+  alert("room.");
+}
+function getStatement2() {
+  alert("drawing board.");
+}
+function getStatement3() {
+  alert("cat.");
+}
+function getStatement4() {
+  alert("cake.");
+}
+function getStatement5() {
+  alert("must come down.");
+}
+
+document.getElementById("statement1").addEventListener("click", getStatement1);
+document.getElementById("statement2").addEventListener("click", getStatement2);
+document.getElementById("statement3").addEventListener("click", getStatement3);
+document.getElementById("statement4").addEventListener("click", getStatement4);
+document.getElementById("statement5").addEventListener("click", getStatement5);
